9a8d0ed6a3c7684405efba208bcc6751f1ad2466
phil-ogutu <phil.shifwoko@gmail.com>
2025-03-23 20:21:34 +0300

    DOM modifying

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainElement = document.querySelector("main#main")
+mainElement.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Phil Ogutu is the champion"
